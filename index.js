bab0e864c48cea57df1806eecc884b0c7d61295e
German Laurito <germanlauritopass14@gmail.com>
2026-09-22 11:11:59 -0300

    Ignorar carpeta sf

---
+++ index.js
@@ -0,0 +1,7 @@
+function linkedin() {
+    window.open('https://www.linkedin.com/in/german-laurito-997684190/', '_blank', 'noopener');
+}
+
+function git() {
+    window.open('https://github.com/GermanLP14?tab=repositories', '_blank', 'noopener');
+}
